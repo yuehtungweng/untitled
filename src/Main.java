66971d6026ae983bfb8fcadb71fabb55de563b15
I432 <to880619@gmail.com>
2017-09-25 15:15:40 +0800

    10690925

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("My First Java Code!!        ");
+        System.out.print("My 2 Java Code!!        ");
 
     }
 }
